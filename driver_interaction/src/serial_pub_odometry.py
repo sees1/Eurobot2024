0e4ecf51e820a69274c0efa3ca66ce17b313cdbd
Sees <Vsevolod.Egorov@ronavi.ru>
2024-05-15 01:22:12 +0300

    try to reach 30 hz part 6

--- driver_interaction/src/serial_pub_odometry.py
+++ driver_interaction/src/serial_pub_odometry.py
@@ -26,7 +26,7 @@ class Odom():
 
         self.odom_pub   = rospy.Publisher("/odom", Odometry, queue_size = 1)
         self.start_pub  = rospy.Publisher("/start_engine", Bool, queue_size = 1)
-        self.ser        = serial.Serial('/dev/ttyACM0', 28800, timeout = 1.0)
+        self.ser        = serial.Serial('/dev/ttyACM0', 28800, timeout = None)
 
         self.start_flag = Bool()
 
